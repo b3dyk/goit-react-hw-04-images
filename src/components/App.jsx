f30eab19339d6199fcdf6d2089106acc50d8f676
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-01-13 14:10:19 +0200

    refactor to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
@@ -8,141 +8,117 @@ import { getImages } from 'services/images.service';
 import { STATUS } from 'constants/status.constants';
 import Notify from 'services/notifications.service';
 
-export class App extends Component {
-  state = {
-    images: [],
-    page: 1,
-    search: '',
-    currentImage: null,
-    status: STATUS.idle,
-  };
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
+  const [currentImage, setCurrentImage] = useState(null);
+  const [status, setStatus] = useState(STATUS.idle);
 
-  componentDidUpdate(_, prevState) {
-    const { page, search } = this.state;
-    if (prevState.page !== page || prevState.search !== search) {
-      if (search !== '') {
-        this.fetchData({ page, search });
+  useEffect(() => {
+    const fetchData = async ({ page, search = '' }) => {
+      setStatus(STATUS.loading);
+
+      try {
+        const data = await getImages({ page, search });
+        handleResolve(data);
+      } catch (error) {
+        setStatus(STATUS.error);
+        console.log(error);
       }
-    }
-  }
+    };
 
-  handleSearch = search => {
-    if (search === '') {
+    const handleResolve = ({ hits, total, totalHits }) => {
+      const sortedImages = hits.map(
+        ({ id, webformatURL, tags, largeImageURL }) => ({
+          id,
+          webformatURL,
+          tags,
+          largeImageURL,
+        })
+      );
+
+      if (!total) {
+        Notify.failure(
+          'Sorry, there are no images matching your search, please try another key word'
+        );
+        setStatus(STATUS.idle);
+        return;
+      }
+
+      if (totalHits < page * 12) {
+        setImages(prevState => [...prevState, ...sortedImages]);
+        setStatus(STATUS.idle);
+        Notify.failure(
+          "We're sorry, but you've reached the end of search results"
+        );
+        return;
+      }
+
+      setImages(prevState => [...prevState, ...sortedImages]);
+      setStatus(STATUS.success);
+
+      if (page === 1) {
+        Notify.success(`That's what we found`);
+      }
+    };
+
+    if (!search) {
+      return;
+    }
+
+    fetchData({ page, search });
+  }, [page, search]);
+
+  const handleSearch = search => {
+    if (!search) {
       Notify.warning('Please enter key word');
     }
-    this.setState({
-      images: [],
-      search,
-      page: 1,
-    });
+    setSearch(search);
+    setImages([]);
+    setPage(1);
   };
 
-  fetchData = async ({ page = this.state.page, search = '' }) => {
-    this.setState({ status: STATUS.loading });
-
-    try {
-      const data = await getImages({ page, search });
-      this.handleResolve(data);
-    } catch (error) {
-      this.setState({ status: STATUS.error });
-      console.log(error);
-    }
-  };
-
-  handleResolve = ({ hits, total, totalHits }) => {
-    const sortedImages = hits.map(
-      ({ id, webformatURL, tags, largeImageURL }) => ({
-        id,
-        webformatURL,
-        tags,
-        largeImageURL,
-      })
-    );
-
-    if (!total) {
-      Notify.failure(
-        'Sorry, there are no images matching your search, please try another key word'
-      );
-      this.setState({ status: STATUS.idle });
-      return;
-    }
-
-    if (totalHits < this.state.page * 12) {
-      this.setState(({ images }) => ({
-        images: [...images, ...sortedImages],
-        status: STATUS.idle,
-      }));
-      Notify.failure(
-        "We're sorry, but you've reached the end of search results"
-      );
-      return;
-    }
-
-    this.setState(({ images }) => ({
-      images: [...images, ...sortedImages],
-      status: STATUS.success,
-    }));
-    if (this.state.page === 1) {
-      Notify.success(`That's what we found`);
-    }
-  };
-
-  handleLoadMore = () => {
-    this.setState(({ page }) => ({
-      page: page + 1,
-    }));
+  const handleLoadMore = () => {
+    setPage(prevState => prevState + 1);
     Notify.success('Here you have more pictures');
   };
 
-  openModal = image => {
-    this.setState({ currentImage: image });
-  };
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '1fr',
+        gridGap: '16px',
+        paddingBottom: '24px',
+        color: '#122236',
+        fontFamily: 'Verdana, Geneva, Tahoma, sans-serif',
+        fontSize: '24px',
+      }}
+    >
+      <Searchbar onSubmit={handleSearch} />
 
-  closeModal = () => {
-    this.setState({ currentImage: null });
-  };
+      {images.length > 0 && (
+        <ImageGallery images={images} onClick={setCurrentImage} />
+      )}
 
-  render() {
-    const { images, status, currentImage } = this.state;
+      {images.length > 0 && status === STATUS.success && (
+        <Button onClick={handleLoadMore} />
+      )}
 
-    return (
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr',
-          gridGap: '16px',
-          paddingBottom: '24px',
-          color: '#122236',
-          fontFamily: 'Verdana, Geneva, Tahoma, sans-serif',
-          fontSize: '24px',
-        }}
-      >
-        <Searchbar onSubmit={this.handleSearch} />
+      {status === STATUS.loading && <Loader />}
 
-        {images.length > 0 && (
-          <ImageGallery images={images} onClick={this.openModal} />
-        )}
+      {currentImage && <Modal image={currentImage} onClose={setCurrentImage} />}
 
-        {images.length > 0 && status === STATUS.success && (
-          <Button onClick={this.handleLoadMore} />
-        )}
-
-        {status === STATUS.loading && <Loader />}
-
-        {currentImage && (
-          <Modal image={currentImage} onClose={this.closeModal} />
-        )}
-
-        {status === STATUS.error && (
-          <h2
-            style={{
-              textAlign: 'center',
-            }}
-          >
-            Oops... Something went wrong
-          </h2>
-        )}
-      </div>
-    );
-  }
-}
+      {status === STATUS.error && (
+        <h2
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Oops... Something went wrong
+        </h2>
+      )}
+    </div>
+  );
+};
